624a8e7638ae042ea70b45bcbefed04b5fa4f8cd
sayuru9 <sayurugunathilaka9@gmail.com>
2021-09-25 10:10:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+function App() {
+
+  const element = <h1>Hello, world!</h1>;
+
+  return (
+    <div>
+      {element}
+    </div>
+  );
+}
+
+export default App;
